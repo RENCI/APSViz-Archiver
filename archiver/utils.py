25963a4f231941cb770636075776c27b2a3faf55
Phil Owen <19691521+PhillipsOwen@users.noreply.github.com>
2022-08-05 12:37:28 -0400

    updates to the slack msgs, using correct package, refactoring for better error handling

--- archiver/utils.py
+++ archiver/utils.py
@@ -5,7 +5,8 @@
 # SPDX-License-Identifier: MIT
 
 import os
-import slack
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
 
 
 class Utils:
@@ -18,64 +19,40 @@ class Utils:
         self.logger = logger
 
         # instantiate slack connectivity
-        self.slack_status_client = slack.WebClient(token=os.getenv('SLACK_STATUS_TOKEN'))
         self.slack_status_channel = os.getenv('SLACK_STATUS_CHANNEL')
-        self.slack_issues_client = slack.WebClient(token=os.getenv('SLACK_ISSUES_TOKEN'))
         self.slack_issues_channel = os.getenv('SLACK_ISSUES_CHANNEL')
 
         # get the environment this instance is running on
         self.system = os.getenv('SYSTEM', 'System name not set')
 
-    def send_slack_status_msg(self, run_id, msg, debug_mode=False, instance_name=None):
+    def send_slack_msg(self, run_id, msg, channel, debug_mode=False):
         """
         sends a msg to the Slack status channel
 
         :param run_id:
         :param msg:
+        :param channel:
         :param debug_mode:
-        :param instance_name:
         :return:
         """
+        # if the channel is empty default to the status channel
+        if channel == self.slack_status_channel:
+            client = WebClient(token=os.getenv('SLACK_STATUS_TOKEN'))
+        else:
+            client = WebClient(token=os.getenv('SLACK_ISSUES_TOKEN'))
+
         # init the final msg
         final_msg = f"APSViz Archiver ({self.system}) - "
 
-        # if there was an instance name use it
-        if instance_name is not None:
-            final_msg += f'Instance name: ' + instance_name + ', '
-
         # add the run id and msg
-        final_msg += f"Run ID: {run_id} {msg}"
+        final_msg += f"{msg}"
 
-        # log the message if in debug mode
-        if debug_mode:
-            self.logger.info(final_msg)
-        # else send the message to slack
-        else:
-            self.slack_status_client.chat_postMessage(channel=self.slack_status_channel, text=final_msg)
+        # log the message
+        self.logger.info(final_msg)
 
-    def send_slack_issues_msg(self, run_id, msg, debug_mode=False, instance_name=None):
-        """
-        sends a msg to the Slack issues channel
-
-        :param run_id:
-        :param msg:
-        :param debug_mode:
-        :param instance_name:
-        :return:
-        """
-        # init the final msg
-        final_msg = f"APSViz Archiver ({self.system}) - "
-
-        # if there was an instance name use it
-        if instance_name is not None:
-            final_msg += f'Instance name: ' + instance_name + ', '
-
-        # add the run id and msg
-        final_msg += f"Run ID: {run_id} {msg}"
-
-        # log the message if in debug mode
-        if debug_mode:
-            self.logger.info(final_msg)
-        # else send the message to slack
-        else:
-            self.slack_issues_client.chat_postMessage(channel=self.slack_issues_channel, text=final_msg)
+        try:
+            # send the message to slack if not in debug mode
+            if not debug_mode and self.system in ['Dev', 'Prod']:
+                result = self.slack_status_client.chat_postMessage(channel=channel, text=final_msg)
+        except SlackApiError as e:
+            self.logger.exception(f'Slack {channel} messaging failed. msg: {final_msg}')
