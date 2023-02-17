af1144f72fa2f862bfd3b5f60558a7ed1a6ed51b
Phil Owen <19691521+PhillipsOwen@users.noreply.github.com>
2023-02-17 08:56:40 -0500

    fixing CRLF

---
+++ test/test_geoserver_ops.py
@@ -0,0 +1,108 @@
+# SPDX-FileCopyrightText: 2022 Renaissance Computing Institute. All rights reserved.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+# SPDX-License-Identifier: LicenseRef-RENCI
+# SPDX-License-Identifier: MIT
+
+"""
+    Test geoserver operations
+
+    Author: Phil Owen, 2/16/2023
+"""
+
+from src.common.geoserver_utils import GeoServerUtils
+
+# init the coverage store name used for all these tests
+coverage_store_name: str = 'test_coverage_store'
+
+
+def test_create_coverage_store():
+    """
+    Tests the creation of a geoserver coverage store
+
+    :return:
+    """
+    # get a handle to the geoserver utils
+    geo_svr: GeoServerUtils = GeoServerUtils()
+
+    # create the coverage store
+    success: bool = geo_svr.create_coverage_store(coverage_store_name)
+
+    # check the result
+    assert success
+
+
+def test_get_coverage_store():
+    """
+    Tests the retrieval of a geoserver coverage store
+
+    :return:
+    """
+    # get a handle to the geoserver utils
+    geo_svr: GeoServerUtils = GeoServerUtils()
+
+    # get the coverage store
+    success, store = geo_svr.get_coverage_store('test_coverage_store')
+
+    # check the result
+    assert success and len(store) == 1
+
+    # get the layer list
+    success, store = geo_svr.get_coverage_store('this is a invalid coverage store name')
+
+    # check the result
+    assert not success and store.text.startswith('No such coverage store: ')
+
+
+def test_get_coverage_stores():
+    """
+    Tests getting all the geoserver coverage stores
+
+    :return:
+    """
+    # get a handle to the geoserver utils
+    geo_svr: GeoServerUtils = GeoServerUtils()
+
+    # get the coverage stores list
+    success, stores = geo_svr.get_coverage_stores()
+
+    # make sure we got some back
+    assert len(stores['coverageStores']['coverageStore']) > 1
+
+    # init the new coverage store name found flag
+    found = False
+
+    # look for the one we added above
+    for item in stores['coverageStores']['coverageStore']:
+        # was it found
+        if item['name'].startswith(coverage_store_name):
+            # set the flag
+            found = True
+
+            # no need to continue
+            break
+
+    # check the results
+    assert success and found
+
+
+def test_remove_coverage_store():
+    """
+    Tests the deletion of a geoserver coverage store
+
+    :return:
+    """
+    # get a handle to the geoserver utils
+    geo_svr: GeoServerUtils = GeoServerUtils()
+
+    # remove the coverage store
+    success: bool = geo_svr.remove_coverage_store(coverage_store_name)
+
+    # check the result
+    assert success
+
+    # get the layer list
+    success, store = geo_svr.get_coverage_store(coverage_store_name)
+
+    # check the result
+    assert not success and store.text.startswith('No such coverage store: ')
---
+++ test/test_utils.py
@@ -0,0 +1,148 @@
+# SPDX-FileCopyrightText: 2022 Renaissance Computing Institute. All rights reserved.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+# SPDX-License-Identifier: LicenseRef-RENCI
+# SPDX-License-Identifier: MIT
+
+"""
+    Test Utils - Utilities used for testing
+
+    Author: Phil Owen, 10/23/2022
+"""
+import os.path
+from time import sleep
+
+from src.archiver.rule_handler import RuleHandler
+from src.common.rule_utils import RuleUtils
+
+# define the directory for the tests and data
+input_path = os.path.dirname(__file__)
+output_path = os.getenv('TESTDATA_PATH', os.path.dirname(__file__))
+
+
+def sweeps_init():
+    """
+    creates test data for this series of tests
+
+    :return:
+    """
+    # source location of all test files
+    source: str = os.path.join(input_path, 'test_files')
+
+    # source location of a single test file
+    source_file: str = os.path.join(source, 'test_file.txt')
+
+    # location of destination directory
+    dest_dir: str = os.path.join(output_path, 'sweep_dir1/')
+
+    # location of a subdirectory
+    dest_sub: str = os.path.join(dest_dir, 'sweep_sub/')
+
+    # prep for directory sweep tests
+    test_rules: list = [
+        {'name': 'Test - Copy Test file', 'description': '', 'query_criteria_type': None, 'query_data_type': None, 'query_data_value': 1,
+         'predicate_type': None, 'sync_system_type': None, 'action_type': 'COPY', 'data_type': 'FILE', 'source': source_file,
+         'destination': dest_dir},
+        {'name': 'Test - Copy directory', 'description': 'Directory creation.', 'query_criteria_type': None, 'query_data_type': None,
+         'query_data_value': None, 'predicate_type': None, 'sync_system_type': None, 'action_type': 'COPY', 'data_type': 'DIRECTORY',
+         'source': source, 'destination': dest_sub}]
+
+    # create the run handler
+    rule_handler = RuleHandler()
+
+    # for each test
+    for test_rule in test_rules:
+        # create a rule utility
+        rule_utils = RuleUtils()
+
+        # validate and convert the dict into a rule
+        rule: RuleUtils.Rule = rule_utils.validate_and_convert_to_rule(test_rule)
+
+        # check the rule
+        assert rule
+
+        # apply the rules to the data, get back some stats
+        process_stats: dict = rule_handler.process_rule(rule)
+
+        # interrogate the result
+        assert process_stats['copied'] == 1 and process_stats['failed'] == 0
+
+        assert os.path.isfile(os.path.join(dest_dir, 'test_file.txt'))
+        assert os.path.exists(os.path.join(dest_dir, 'sweep_sub'))
+
+        # get a list of the contents of the source directory
+        entities = os.listdir(source)
+
+        # make sure all the files were transferred
+        for entity in entities:
+            assert os.path.isfile(os.path.join(dest_sub, entity))
+
+    # pause so that the data ages long enough so age tests work
+    sleep(3)
+
+
+def cleanup(dirs: list):
+    """
+    Cleans up the leftover directories
+
+    :return:
+    """
+    for target_dir in dirs:
+        # get the paths to the test directories
+        source_dir: str = os.path.join(output_path, target_dir)
+
+        # create a test rule
+        test_rule: dict = {'name': 'Test - Remove file', 'description': 'Remove file', 'query_criteria_type': None, 'query_data_type': None,
+                           'query_data_value': None, 'predicate_type': None, 'sync_system_type': None, 'action_type': 'REMOVE',
+                           'data_type': 'DIRECTORY', 'source': source_dir, 'destination': None}
+
+        # create the run handler
+        rule_handler = RuleHandler()
+
+        # create a rule utility
+        rule_utils = RuleUtils()
+
+        # convert the dict into a rule
+        rule: RuleUtils.Rule = rule_utils.validate_and_convert_to_rule(test_rule)
+
+        # check the rule
+        assert rule
+
+        # apply the rules to the data, get back some stats
+        process_stats: dict = rule_handler.process_rule(rule)
+
+        # interrogate the result
+        assert process_stats['removed'] == 1 and process_stats['failed'] == 0
+
+        # interrogate the result
+        assert not os.path.exists(source_dir)
+
+
+def run_rule(test_rule: dict):
+    """
+    Validates and runs a rule
+
+    :param test_rule:
+    :return:
+    """
+    # init the return
+    process_stats: dict = {'moved': 0, 'copied': 0, 'removed': 0, 'swept': 0, 'failed': 0}
+
+    # create a rule utility
+    rule_utils = RuleUtils()
+
+    # validate and convert the dict into a rule
+    rule: RuleUtils.Rule = rule_utils.validate_and_convert_to_rule(test_rule)
+
+    # make sure this is a valid rule
+    if rule:
+        # create the run handler
+        rule_handler = RuleHandler()
+
+        # apply the rules to the data, get back some stats
+        process_stats = rule_handler.process_rule(rule)
+    else:
+        process_stats['failed'] += 1
+
+    # return to the caller
+    return process_stats
