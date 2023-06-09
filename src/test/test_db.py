4207dfc1143c3e37e65950205c8472db80b46cd7
Phil Owen <19691521+PhillipsOwen@users.noreply.github.com>
2023-06-09 15:07:01 -0400

    adding test for the is_tropical_run SP

--- src/test/test_db.py
+++ src/test/test_db.py
@@ -77,3 +77,36 @@ def test_remove_adcirc_obs_stations():
 
     # check to make sure only one was removed
     assert ret_val == 1
+
+
+@pytest.mark.skip(reason="Local test only")
+def test_is_tropical():
+    """
+    test the SP that determines if the run is tropical (or synoptic)
+
+    :return:
+    """
+    # specify the DB to get a connection
+    # note the extra comma makes this single item a singleton tuple
+    db_name: tuple = ('apsviz',)
+
+    # create a DB connection object
+    db_info = PGImplementation(db_name)
+
+    # check the object returned
+    assert len(db_info.dbs) == len(db_name)
+
+    # make a failure call
+    ret_val = db_info.is_tropical_run("invalid instance id")
+
+    assert not ret_val
+
+    # make a good call for a synoptic run
+    ret_val = db_info.is_tropical_run('4356-2023042018-nowcast')
+
+    assert not ret_val
+
+    # make a good call to a tropical run
+    ret_val = db_info.is_tropical_run('4397-031-nowcast')
+
+    assert ret_val
