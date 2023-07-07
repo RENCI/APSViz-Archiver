a53116a85b97ce155a734135c7598476479be4aa
Phil Owen <19691521+PhillipsOwen@users.noreply.github.com>
2023-07-07 11:54:28 -0400

    adding test to check the existence of the env params

---
+++ src/test/test_env_params.py
@@ -0,0 +1,26 @@
+# SPDX-FileCopyrightText: 2023 Renaissance Computing Institute. All rights reserved.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+# SPDX-License-Identifier: LicenseRef-RENCI
+# SPDX-License-Identifier: MIT
+
+"""
+    APSViz Archiver - Test directory operations
+
+    Author: Phil Owen, 07/07/2023
+"""
+import os
+
+
+def test_env_params():
+    """
+    tests that the needed environment params are installed.
+
+    :return:
+    """
+    # get a list of the target params
+    env_params: list = ['FILESERVER_OBS_PATH', 'GEOSERVER_HOST', 'GEOSERVER_PASSWORD', 'GEOSERVER_PROJ_PATH', 'GEOSERVER_URL', 'GEOSERVER_USER', 'GEOSERVER_WORKSPACE']
+
+    # for each param
+    for item in env_params:
+        assert os.getenv(item)
