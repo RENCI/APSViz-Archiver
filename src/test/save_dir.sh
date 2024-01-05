cdce1d5b8cf51916159f620e25fb0b175fb5b7c2
Phil Owen <19691521+PhillipsOwen@users.noreply.github.com>
2024-01-05 10:31:37 -0500

    updating licensing

--- src/test/save_dir.sh
+++ src/test/save_dir.sh
@@ -1,3 +1,9 @@
+# SPDX-FileCopyrightText: 2024 Renaissance Computing Institute. All rights reserved.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+# SPDX-License-Identifier: LicenseRef-RENCI
+# SPDX-License-Identifier: MIT
+
 #/bin/bash
 now=$(date +"%m-%d-%Y")               
 ls -halt /data/logs >> log_dir_list_$now.txt
