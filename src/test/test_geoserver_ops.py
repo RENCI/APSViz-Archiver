e5e7222d4ed278b45f88a2cdf29d4d5c2f399b53
Phil Owen <19691521+PhillipsOwen@users.noreply.github.com>
2024-02-09 11:40:01 -0500

    refactoring for clarity, adding TDS configuration

--- src/test/test_geoserver_ops.py
+++ src/test/test_geoserver_ops.py
@@ -11,12 +11,10 @@
 
     Author: Phil Owen, 2/16/2023
 """
-# import pytest
 import os
-from collections import namedtuple
-
 import pytest
 
+from collections import namedtuple
 from src.test.test_utils import run_rule
 from src.common.geoserver_utils import GeoServerUtils
 from src.common.rule_utils import RuleUtils
@@ -24,60 +22,89 @@ from src.common.rule_utils import RuleUtils
 # init the full coverage store name (aka instance id e.g. 4303-2023020206-namforecast_maxele63)
 instance_id: str = 'test_store'
 
-# get the geoserver workspace name
-geoserver_workspace: str = os.environ.get('GEOSERVER_WORKSPACE', 'ADCIRC_2024')
-
-# get the working directory name
-input_path = os.path.dirname(__file__)
-
-# make the dir if it doesn't exist
-if input_path and not os.path.exists(input_path):
-    os.makedirs(input_path)
-
-# get a test directory for the geoserver files
-geoserver_proj_path: str = os.environ.get('GEOSERVER_PROJ_PATH', 'pytest_geoserver_data')
-
-assert input_path
-assert geoserver_proj_path
-
-# get the base path of the test data
-base_path = os.path.join(input_path, geoserver_proj_path)
-
-# save the new base path
-os.environ['GEOSERVER_PROJ_PATH'] = base_path
-
-# get a test directory for obs data files
-obs_proj_path: str = os.environ.get('FILESERVER_OBS_PATH', 'obsmod_data')
-
-# get the location of the obs mod data
-obs_proj_dir = os.path.join(base_path, obs_proj_path)
-
-# save the new obs data path
-os.environ['FILESERVER_OBS_PATH'] = obs_proj_dir
-
-# source location of all test files
-source_dir: str = os.path.join(base_path, geoserver_workspace)
-
-# location of destination directory
-dest_dir: str = os.path.join(base_path, 'geoserver_out')
-
-# check if the geoserver directory exists
-if not os.path.exists(source_dir):
-    # create the geoserver directory
-    os.makedirs(source_dir)
-
-# check if the obsmod directory exists
-if not os.path.exists(dest_dir):
-    # create the obs/mod directory
-    os.makedirs(dest_dir)
-
-# check if the obsmod directory exists
-if not os.path.exists(obs_proj_dir):
-    # create the obs/mod directory
-    os.makedirs(obs_proj_dir)
-
 # set the global test mode
 test_mode: bool = True
+
+# declare global variables
+geoserver_workspace: str = ''
+geoserver_proj_path: str = ''
+obs_proj_dir: str = ''
+source_dir : str = ''
+dest_dir: str = ''
+
+
+def setup_dirs_and_envs():
+    """
+    prepares the environment for testing
+
+    :return:
+    """
+    # for all tests to use
+    global obs_proj_dir, source_dir, dest_dir, geoserver_workspace, geoserver_proj_path
+
+    # get the geoserver workspace name
+    geoserver_workspace = os.environ.get('GEOSERVER_WORKSPACE', 'ADCIRC_2024')
+
+    # get the working directory name
+    input_path = os.path.dirname(__file__)
+
+    assert input_path
+
+    # make the dir if it doesn't exist
+    if input_path and not os.path.exists(input_path):
+        os.makedirs(input_path)
+
+    # get a test directory for the geoserver files
+    geoserver_proj_path = os.environ.get('GEOSERVER_PROJ_PATH', 'pytest_geoserver_data')
+
+    assert geoserver_proj_path
+
+    # get the base path of the test data
+    base_path = os.path.join(input_path, geoserver_proj_path)
+
+    # save the new base path
+    os.environ['GEOSERVER_PROJ_PATH'] = base_path
+
+    # get a test directory for obs data files
+    obs_proj_path = os.environ.get('FILESERVER_OBS_PATH', 'obsmod_data')
+
+    # get the location of the obs mod data
+    obs_proj_dir = os.path.join(base_path, obs_proj_path)
+
+    # save the new obs data path
+    os.environ['FILESERVER_OBS_PATH'] = obs_proj_dir
+
+    # save the TDS data path
+    os.environ['TDS_BASE_PATH'] = base_path
+
+    # set the TDS server url. this must be in sync with the namespace the geoserver is in
+    # it must match the location of the TDS test data. anything else will not pe processed
+    os.environ['TDS_URL'] = os.environ.get('TDS_URL', 'https://tds.renci.org/thredds/fileServer/')
+
+    # source location of all test files
+    source_dir = os.path.join(base_path, geoserver_workspace)
+
+    # location of destination directory
+    dest_dir = os.path.join(base_path, 'geoserver_out')
+
+    # check if the geoserver directory exists
+    if not os.path.exists(source_dir):
+        # create the geoserver directory
+        os.makedirs(source_dir)
+
+    # check if the obsmod directory exists
+    if not os.path.exists(dest_dir):
+        # create the obs/mod directory
+        os.makedirs(dest_dir)
+
+    # check if the obsmod directory exists
+    if not os.path.exists(obs_proj_dir):
+        # create the obs/mod directory
+        os.makedirs(obs_proj_dir)
+
+
+# setup for testing
+setup_dirs_and_envs()
 
 
 def create_geoserver_store(store_type: str) -> bool:
@@ -155,7 +182,7 @@ def create_test_dirs(start: int, stop: int, max_count: int):
     # start a counter
     count: int = 0
 
-    # did we get data
+    # did we get data?
     if len(stores) != 0:
         # loop through the stores
         for store in stores[start:stop]:
@@ -185,7 +212,7 @@ def create_test_dirs(start: int, stop: int, max_count: int):
                 os.mkdir(full_obs_path)
 
 
-@pytest.mark.skip(reason="Local test only")
+#@pytest.mark.skip(reason="Local test only")
 def test_geoserver_remove_rule():
     """
     tests the retrieval of geoserver entries that meet rule criteria
